36ce2557fb364e0399d202715b552cf45e7ee950
Rahat Ahmed Khan <rahatofficial.29@gmail.com>
2026-05-18 20:44:39 +0600

    feat:postgres integration

--- server/app/config.py
+++ server/app/config.py
@@ -11,10 +11,10 @@ class Settings(BaseSettings):
     )
 
     DATABASE_URL: str
-    ACCESS_SECRET_KEY: str
-    REFRESH_SECRET_KEY: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    REFRESH_TOKEN_EXPIRE_DAYS:INT
+    # ACCESS_SECRET_KEY: str
+    # REFRESH_SECRET_KEY: str
+    # ACCESS_TOKEN_EXPIRE_MINUTES: int
+    # REFRESH_TOKEN_EXPIRE_DAYS:int
 
 
 settings = Settings()
--- server/app/main.py
+++ server/app/main.py
@@ -1,3 +1,20 @@
 # Entry point for the FastAPI application.
 # Registers all routers and configures CORS.
 # Keep this file thin — no business logic here.
+from fastapi import FastAPI
+# from app.routes.auth import router as auth_router
+from app.database.base import Base
+from app.database.session import engine
+
+app = FastAPI()
+
+# Create tables automatically
+Base.metadata.create_all(bind=engine)
+
+# include routes
+# app.include_router(auth_router)
+
+
+@app.get("/")
+def home():
+    return {"message": "Mentree AI API Running"}
